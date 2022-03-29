c6d9f095e9a7842158d711a61c0242b155cf0ad4
Luis Duarte <contact.gmail.com>
2022-03-28 22:06:30 -0400

    "este es mi segundo commit"

--- No_tiene_nada/Program.cs
+++ No_tiene_nada/Program.cs
@@ -7,6 +7,7 @@ namespace No_tiene_nada
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo");
         }
     }
 }
