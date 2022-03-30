94450ac96e12b98986f3b302f3f458c25c0359b2
Luis Duarte <contact.gmail.com>
2022-03-30 16:02:03 -0400

    esta es mi primera version perfecta

--- No_tiene_nada/Program.cs
+++ No_tiene_nada/Program.cs
@@ -6,7 +6,19 @@ namespace No_tiene_nada
     {
         static void Main(string[] args)
         {
+
+            int numero=1;
+            int numero2=2;
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo");
+            if (numero > numero2)
+            {
+                Console.WriteLine("hola como estas");
+            }
+            else
+            {
+                Console.WriteLine("No estoy bien");
+            }
         }
     }
 }
